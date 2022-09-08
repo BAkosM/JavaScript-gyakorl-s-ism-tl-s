1a10960bfdeb9d1449d68f10f1750e6117e5e8f8
Kecseti Péter <peter.kecseti@gmail.com>
2022-09-08 12:40:15 +0200

    3.a Feladat

--- feladat3.js
+++ feladat3.js
@@ -1,1 +1,4 @@
-setInterval(() => {  console.log("Hurrá!"); }, 2000);
+//3.Feladat
+var i = 0;
+//setInterval(() => {  console.log("Hurrá!"); }, 2000);
+setInterval(() => {  console.log(i);i++; }, 2000);
